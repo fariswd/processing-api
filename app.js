53e6db383719fdc180cb31308867c8139bcb0b6a
Faris Widyantho <radenfaris@gmail.com>
2018-04-12 22:07:57 +0700

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const app = require('express')()
 const mongoose = require('mongoose').connect(process.env.DB)
+const cors = require('cors')
+const morgan = require('morgan')
+
+app.use(cors())
+app.use(morgan('dev'))
 
 const v1Router  = require('./routes/v1Routes')
 
